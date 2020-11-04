310749db1080a29bf6c7d420172f66dac5bbcf30
Mikko Koskinen <Mikko.Koskinen@sulava.com>
2020-11-04 06:48:07 +0200

    Bug fixes.

--- CreateBaconPage/CustomModules/EnterpiseAPI.psm1
+++ CreateBaconPage/CustomModules/EnterpiseAPI.psm1
@@ -12,7 +12,7 @@ function GiveMeBacon{
     $response = ""
 
     try {
-        $queryURL = ("https://baconipsum.com/api/?&paras={0}&start-with-lorem=1&format=text" -f $meatParas)
+        $queryURL = ("https://baconipsum.com/api/?type=all-meat&paras={0}&start-with-lorem=1&format=text" -f $meatParas)
         
         $response = Invoke-RestMethod -Uri $queryURL -ContentType "application/json; charset=utf-8" -Method Post -UseBasicParsing
     }
